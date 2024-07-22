c2b247e7370037570cb6a3b87e606cf60179d5f3
Owais-Alshishani <Awais.alshishani@gmail.com>
2024-07-22 16:09:38 +0300

    Added CRUD operations
    check the book for more details

--- WorkingWithEFCore/Program.cs
+++ WorkingWithEFCore/Program.cs
@@ -11,4 +11,61 @@
 //WorkingWithEFCore.Program.QueryingWithLike();
 //WorkingWithEFCore.Program.GetRandomProduct();
 //WorkingWithEFCore.Program.GettingOneProduct();
-WorkingWithEFCore.Program.LazyLoadingWithNoTracking();
+//WorkingWithEFCore.Program.LazyLoadingWithNoTracking();
+
+//---------------
+
+//var resultAdd = WorkingWithEFCore.Program.AddProduct(categoryId: 6, productName: "Bob's Burgers", price: 250M, stock: 40);
+//if (resultAdd.affected == 1)
+//{
+//    WriteLine($"Add product successful with ID: {resultAdd.productId}.");
+//}
+//WorkingWithEFCore.Program.ListProducts(productIdsToHighlight: new[] { resultAdd.productId });
+
+//---------------
+
+//var resultUpdate = WorkingWithEFCore.Program.IncreaseProductPrice(
+// productNameStartsWith: "Bob", amount: 20M);
+//if (resultUpdate.affected == 1)
+//{
+//    WriteLine($"Increase price success for ID: {resultUpdate.productId}.");
+//}
+//WorkingWithEFCore.Program.ListProducts(productIdsToHighlight: new[] { resultUpdate.productId });
+
+//---------------
+
+//WriteLine("About to delete all products whose name starts with Bob.");
+//Write("Press Enter to continue or any other key to exit: ");
+//if (ReadKey(intercept: true).Key == ConsoleKey.Enter)
+//{
+//    int deleted = WorkingWithEFCore.Program.DeleteProducts(productNameStartsWith: "Bob");
+//    WriteLine($"{deleted} product(s) were deleted.");
+//}
+//else
+//{
+//    WriteLine("Delete was canceled.");
+//}
+
+//var resultUpdateBetter = WorkingWithEFCore.Program.IncreaseProductPricesBetter(
+// productNameStartsWith: "Bob", amount: 20M);
+//if (resultUpdateBetter.affected > 0)
+//{
+//    WriteLine("Increase product price successful.");
+//}
+//WorkingWithEFCore.Program.ListProducts(productIdsToHighlight: resultUpdateBetter.productIds);
+
+
+//---------------
+
+
+WriteLine("About to delete all products whose name starts with Bob.");
+Write("Press Enter to continue or any other key to exit: ");
+if (ReadKey(intercept: true).Key == ConsoleKey.Enter)
+{
+    int deleted = WorkingWithEFCore.Program.DeleteProductsBetter(productNameStartsWith: "Bob");
+    WriteLine($"{deleted} product(s) were deleted.");
+}
+else
+{
+    WriteLine("Delete was canceled.");
+}
